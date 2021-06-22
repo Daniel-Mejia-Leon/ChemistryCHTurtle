6154c869d5a263b116ab14da79d4b177a303ed1d
Daniel-Mejia-Leon <draulmdl@gmaillcom>
2021-06-23 07:17:53 +1100

    ylsDone

--- CH.py
+++ CH.py
@@ -4,29 +4,29 @@ from time import sleep
 count = 1
 
 
-def turn(direction, sustituyente):
-    if not sustituyente and direction == 'Left':
-        turtle.left(90)
-        turtle.forward(50)
-    if sustituyente and direction == 'Left':
-        turtle.left(90)
-        turtle.forward(50)
-        turtle.left(45)
-        turtle.forward(50)
-        turtle.left(180)
-        turtle.forward(50)
-        turtle.left(135)
-    if not sustituyente and direction == 'Right':
-        turtle.right(90)
-        turtle.forward(50)
-    if sustituyente and direction == 'Right':
-        turtle.right(90)
-        turtle.forward(50)
-        turtle.right(45)
-        turtle.forward(50)
-        turtle.right(180)
-        turtle.forward(50)
-        turtle.right(135)
+# def turn(direction, sustituyente):
+#     if not sustituyente and direction == 'Left':
+#         turtle.left(90)
+#         turtle.forward(50)
+#     if sustituyente and direction == 'Left':
+#         turtle.left(90)
+#         turtle.forward(50)
+#         turtle.left(45)
+#         turtle.forward(50)
+#         turtle.left(180)
+#         turtle.forward(50)
+#         turtle.left(135)
+#     if not sustituyente and direction == 'Right':
+#         turtle.right(90)
+#         turtle.forward(50)
+#     if sustituyente and direction == 'Right':
+#         turtle.right(90)
+#         turtle.forward(50)
+#         turtle.right(45)
+#         turtle.forward(50)
+#         turtle.right(180)
+#         turtle.forward(50)
+#         turtle.right(135)
 
 
 def triangulo(up=False, down=False):
@@ -52,27 +52,73 @@ def triangulo(up=False, down=False):
         turtle.forward(50)
         turtle.right(135)
 
+def straight(x):
+    if x == 'Up':
+        turtle.left(90)
+        turtle.forward(50)
+    if x == 'Down':
+        turtle.right(90)
+        turtle.forward(50)
 
 def up_sus(c_number=1):
+    # methyl
     if c_number == 1:
         turtle.left(45)
         turtle.forward(50)
         turtle.left(180)
         turtle.forward(50)
         turtle.left(135)
+
+    # ethyl
     if c_number == 2:
-        turtle.left(45)
+        turtle.left(45)  # start sus
         turtle.forward(50)
         turtle.left(90)
         turtle.forward(50)
-        turtle.left(180)
+        turtle.left(180)  # go back
         turtle.forward(50)
         turtle.right(90)
         turtle.forward(50)
-        turtle.left(135)
+        turtle.left(135)  # finish sus
 
+    # propil
+    if c_number == 3:
+        turtle.left(45)  # start sus
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(180)  # go back
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(135)  # finish sus
+
+    # butyl
+    if c_number == 4:
+        turtle.left(45)  # start sus
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.left(180)  # go back
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(135)  # finish sus
 
 def down_sus(c_number):
+    # methyl
     if c_number == 1:
         turtle.right(45)
         turtle.forward(50)
@@ -80,11 +126,62 @@ def down_sus(c_number):
         turtle.forward(50)
         turtle.right(135)
 
-molecule = '3 - methyl, butane'
+    # ethyl
+    if c_number == 2:
+        turtle.right(45)
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.left(180)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.right(135)
+
+    # propyl
+    if c_number == 3:
+        turtle.right(45)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.left(180)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(135)
+
+    # butyl
+    if c_number == 4:
+        turtle.right(45)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(180)
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.left(90)
+        turtle.forward(50)
+        turtle.right(135)
+
+
+molecule = 'icosane'
 
 # this function is to attach the sus on the main chain
 # the main chain function has the message for invalid sus_loc
 def ch(number, sus_loc=0, sus_len=0, count=1):
+    down = 'Down'
+    up   = 'Up'
     # if the number is 1 dont do anything since methane is just a dot
     if number == 1:
         turtle.left(90)
@@ -96,15 +193,11 @@ def ch(number, sus_loc=0, sus_len=0, count=1):
             turtle.shape('circle')
             turtle.shapesize(0.5)
             break
-        turtle.left(90)
-        turtle.forward(50)
+        straight(up)
         count += 1
         # if sus_loc == count then proceed to create the sus_len sus
         if sus_loc == count:
-            if sus_len == 2:
-                up_sus(2)
-            if sus_len == 1:
-                up_sus()
+            up_sus(sus_len)
         # if sus == count CHECK THIS
         for n in range(number):
             # for number of C
@@ -112,8 +205,7 @@ def ch(number, sus_loc=0, sus_len=0, count=1):
                 turtle.shape('circle')
                 turtle.shapesize(0.5)
                 break
-            turtle.right(90)
-            turtle.forward(50)
+            straight(down)
             count += 1
             # start sus creation in position = count
             if sus_loc == count:
@@ -121,95 +213,44 @@ def ch(number, sus_loc=0, sus_len=0, count=1):
             break
     # variable global count if count == carbonos: stop!
 
+def name_check(x):
+    if met in x:
+        return 1
+    if et in x:
+        return 2
+    if prop in x:
+        return 3
+    if but in x:
+        return 4
+    if pent in x:
+        return 5
+    if hex in x:
+        return 6
+    if hept in x:
+        return 7
+    if oct in x:
+        return 8
+    if non in x:
+        return 9
+    if dec in x:
+        return 10
+    if ico in x:
+        return 20
 
 def chain(x):
     # if the name is just a simple chain
+    prefix = [met, et, prop, but, pent, hex, hept, oct, non, dec, 'Icosane'] # fix this part because we are supposed to use vars not strings
     if len(name) == 1:
-        if met in name[0]:
-            ch(x)
-        if et in name[0]:
-            ch(x)
-        if prop in name[0]:
-            ch(x)
-        if but in name[0]:
-            ch(x)
-        if pent in name[0]:
-            ch(x)
-        if hex in name[0]:
-            ch(x)
-        if hept in name[0]:
-            ch(x)
-        if oct in name[0]:
-            ch(x)
-        if non in name[0]:
-            ch(x)
-        if ico in name[0]:
+        print('llego aqui')
+        if name[0] in prefix:
+            print('aqui tambien')
             ch(x)
     # if the name has 3 characters on it must be sus location-susType-chain#
     if len(name) == 3:
         if name[-3] == 0:
             ch(x)
-        # here is where you have to put the invalid warning for sus_loc == number
-        if int(name[-3]) == 1:
-            print('invalid 1 cant be sus_loc')
-        if int(name[-3]) == 2:
-            if x == 2:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-
-        if int(name[-3]) == 3:
-            if x == 3:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-        if int(name[-3]) == 4:
-            if x == 4:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]), 2)
-        if int(name[-3]) == 5:
-            if x == 5:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-        if int(name[-3]) == 6:
-            if x == 6:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-        if int(name[-3]) == 7:
-            if x == 7:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-        if int(name[-3]) == 8:
-            if x == 8:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-        if int(name[-3]) == 9:
-            if x == 9:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-        if int(name[-3]) == 10:
-            if x == 10:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-        if int(name[-3]) == 11:
-            if x == 11:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-        if int(name[-3]) == 12:
-            if x == 11:
-                print('invalid, sus_loc cant be first nor final ')
-            else:
-                ch(x, int(name[-3]))
-
-
+        #         print('invalid, sus_loc cant be first nor final ')
+        ch(x, int(name[-3]), name_check(name[-2]))
 
 
 name = []
@@ -250,7 +291,7 @@ turtle.shape()
 turtle.shapesize(1.5)
 turtle.goto(-screen_x / 2 + 50, 0)
 turtle.pensize(2)
-turtle.speed(1)
+turtle.speed(2)
 turtle.pendown()
 turtle.right(45)
 
@@ -283,6 +324,9 @@ if oct in name[-1]:
 
 if non in name[-1]:
     chain(9)
+
+if dec in name[-1]:
+    chain(10)
 
 if ico in name[-1]:
     chain(20)
